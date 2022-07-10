88648be3743e3ec29a8f47bfc41cfc40d502aae8
mustafaozturk34 <mustafa.ozturk3408@gmail.com>
2022-07-10 14:03:38 +0300

    update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDwD50U-FHrI4uBPqg95sWv6u6IdeBfQIQ",
+    authDomain: "dinamic-cv.firebaseapp.com",
+    projectId: "dinamic-cv",
+    storageBucket: "dinamic-cv.appspot.com",
+    messagingSenderId: "719109180220",
+    appId: "1:719109180220:web:8e158faebb3294aef46c39",
+    measurementId: "G-FJWDKTVT95"
+  }
 };
